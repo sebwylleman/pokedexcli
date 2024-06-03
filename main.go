670199fe368dbfbf5d42016301eec71da5296303
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-06-03 08:57:24 +0100

    feature: implemented repl exit command

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var pokedexCommands = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exits the pokedex",
+		callback:    commandExit,
+	},
+}
+
+func commandHelp() error {
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Goodbye!")
+	os.Exit(0)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("pokedex > ")
+
+		if !scanner.Scan() {
+			fmt.Println("Error, not able to scan user input. Exiting program.")
+			os.Exit(1)
+		}
+		userInput := scanner.Text()
+		if command, exists := pokedexCommands[userInput]; exists {
+			command.callback()
+		} else {
+			fmt.Println("Invalid command. See Usage")
+		}
+
+	}
 }
